34a3cf9917d3e83427ca658b1eb52157c31c8198
Santiago Robledo <santiddf9@gmail.com>
2025-06-12 19:46:40 -0300

    hice el punto 11

--- js/practica11.js
+++ js/practica11.js
@@ -5,3 +5,26 @@
 //Añadir al ejercicio anterior que el programa también nos diga por cuál o cuáles
 //  de los cuatro números es divisible (es decir, debe mostrar todos los divisores
 //  correspondientes si hay más de uno).
+
+const numero = parseInt(prompt("Introduce un número:"));
+if (isNaN(numero)) {
+    alert("Por favor, introduce un número válido.");
+}
+
+document.writeln("El numero " + numero + " es divisible por: ");
+
+if (numero % 2 === 0) {
+    document.writeln(" 2")
+}
+
+if (numero % 3 === 0) {
+    document.writeln(" 3")
+}
+
+if (numero % 5 === 0) {
+    document.writeln(" 5")
+}
+
+if (numero % 7 === 0) {
+    document.writeln(" 7")
+}
